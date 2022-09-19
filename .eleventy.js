988c9116355602c56c314db2dd31bc073b32eeb1
Randy <randypriv@gmail.com>
2022-09-20 01:44:04 +0800

    add toc

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,13 @@
 const rss = require("@11ty/eleventy-plugin-rss");
 const dayjs = require('dayjs')
 const modern = require('eleventy-plugin-modern')
+const toc = require('markdown-it-toc-done-right')
 module.exports = config => {
-  config.addPlugin(modern)
+  config.addPlugin(modern({
+    markdownIt(md) {
+      md.use(toc)
+    }
+  }))
   config.addPlugin(rss) 
   config.addPassthroughCopy("_redirects");
   config.addPassthroughCopy("favicon.ico");
